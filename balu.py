8142f63f869028b3fc42b6832839aaccdcb0875f
sathwikbalu <dlsathwik@gmail.com>
2024-11-02 23:04:26 +0530

    Add files

--- balu.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-import os
-
-# genai.configure(api_key=os.environ["AIzaSyBtLlboADtSAWc7X4N2hk0nPXkCMOPkR8U"])
-
-model = genai.GenerativeModel("gemini-1.5-flash",api_key="AIzaSyBtLlboADtSAWc7X4N2hk0nPXkCMOPkR8U")
-response = model.generate_content("Write a story about a magic backpack.")
-print(response.text)
